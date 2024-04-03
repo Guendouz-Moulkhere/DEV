4d31300977f1348fe9e8355c9a0651a6302d5c1b
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 18:20:42 -0400

    Update Utilisateur.java

--- src/main/java/com/example/api/Utilisateur.java
+++ src/main/java/com/example/api/Utilisateur.java
@@ -1,7 +1,7 @@
 package com.example.api;
 
 
-
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
