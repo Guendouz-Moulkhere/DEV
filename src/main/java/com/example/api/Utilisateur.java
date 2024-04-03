25fd3adc0352066340d24079aa7eab374ac9d0db
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 17:00:45 -0400

    Update Utilisateur.java

--- src/main/java/com/example/api/Utilisateur.java
+++ src/main/java/com/example/api/Utilisateur.java
@@ -1,13 +1,14 @@
-
 package com.example.api;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 class Utilisateur {
-    @Id @GeneratedValue
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private String email;
