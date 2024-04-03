686a6996696a70fdd02c06d8c971c13bbd0c2514
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 13:58:54 -0400

    Update ApiApplicationTests.java

--- src/test/java/com/example/api/ApiApplicationTests.java
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void helloShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/api/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Bonjour, bienvenue dans votre API Spring Boot!"));
+    }
 }
