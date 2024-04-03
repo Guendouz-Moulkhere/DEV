53bc12efb8f2da48dbf718360792502e19bbdd6f
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 18:07:42 -0400

    Update UtilisateurControllerTests.java

--- src/test/java/com/example/api/UtilisateurControllerTests.java
+++ src/test/java/com/example/api/UtilisateurControllerTests.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,4 +26,3 @@ class UtilisateurControllerTests {
 
     // Ajoutez ici les autres tests concernant l'API des utilisateurs
 }
-
