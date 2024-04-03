64af6ba3fcfa9590df7498961875666e808f51cd
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 14:09:34 -0400

    Update ApiApplication.java

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -18,6 +19,11 @@ public class ApiApplication {
         @GetMapping("/api/hello")
         public String hello() {
             return "Bonjour, bienvenue dans votre API Spring Boot!";
+        }
+
+        @GetMapping("/api/greeting")
+        public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+            return "Bonjour " + name + ", bienvenue dans votre API Spring Boot!";
         }
     }
 }
