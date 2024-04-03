db8b7d4a569497ef420c24bb0b305fb6b94baca8
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 18:05:32 -0400

    Update HelloController.java

--- src/main/java/com/example/api/HelloController.java
+++ src/main/java/com/example/api/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HelloController {
+public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
