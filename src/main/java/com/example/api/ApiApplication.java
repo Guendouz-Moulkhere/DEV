f267b14295d59c7c7891add485df690e03172569
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-03 22:28:30 -0400

    Update ApiApplication.java

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -9,4 +9,13 @@ public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
+
+    @RestController
+    class HelloController {
+
+        @GetMapping("/api/hello")
+        public String hello() {
+            return "Bonjour, bienvenue dans votre API Spring Boot!";
+        }
+    }
 }
